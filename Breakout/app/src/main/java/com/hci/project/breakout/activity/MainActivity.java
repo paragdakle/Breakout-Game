79df2c1526cecc5bd01c5e01c3fd3c0671fe80cd
paragdakle <parag.dakle@gmail.com>
2017-04-20 08:39:01 -0500

    Added basic codebase

---
+++ Breakout/app/src/main/java/com/hci/project/breakout/activity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.hci.project.breakout.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hci.project.breakout.R;
+
+/**
+ * Created by root on 13/4/17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+    }
+}
